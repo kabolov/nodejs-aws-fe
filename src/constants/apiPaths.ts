8414fb6afa82870f872b67b3ba724d0a76707bfe
kabolov <alexander.kabolov@gmail.com>
2020-12-07 19:12:41 +0300

    link fixed

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://kj2o19yhj5.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://wbveo5f0x4.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "http://http://kabolov-cart-api-test.eu-west-1.elasticbeanstalk.com",
+  cart: "http://kabolov-cart-api-test.eu-west-1.elasticbeanstalk.com",
 };
 
 export default API_PATHS;
